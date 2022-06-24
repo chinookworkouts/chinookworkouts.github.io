9b14517daf5c75c93c814fa18aa2c5b25e7358ec
Brendan Weibel <me@bwhybel.com>
2022-06-24 00:01:10 -0500

    getting it to work

--- make_db_call.js
+++ make_db_call.js
@@ -1,11 +1,12 @@
 // save to db
 function saveJson() {
     fetch('https://lively-mud-8fe8731b4065481abf4cebb8e3fcc455.azurewebsites.net/workout', {
-	method: 'POST', // or 'PUT'
+    // fetch('http://127.0.0.1:5000/workout', {
+	method: 'PUT',
 	headers: {
-	    'Content-Type': 'application/json',
+	    'Content-Type': 'application/json'
 	},
-	body: JSON.stringify(globalInfo),
+	body: JSON.stringify(globalInfo)
     })
 	.then(response => response.json())
 	.then(data => {
@@ -19,15 +20,15 @@ function saveJson() {
 // load from db
 function loadJson() {
     url = `https://lively-mud-8fe8731b4065481abf4cebb8e3fcc455.azurewebsites.net/workout?date=${globalInfo.date}&group=${globalInfo.groups.join('')}&time=${globalInfo.time_of_day}`
-    console.log(url);
+    // url = `http://127.0.0.1:5000/workout?date=${globalInfo.date}&group=${globalInfo.groups.join('')}&time=${globalInfo.time_of_day}`
     fetch(url, {
 	method: 'GET',
     })
 	.then(response => response.json())
 	.then(data => {
-	    console.log('Success:', data);
+	    console.log(data);
 	})
 	.catch(error => {
-	    console.log('Error:', error);
+	    console.error('Error:', error);
 	});
 }
